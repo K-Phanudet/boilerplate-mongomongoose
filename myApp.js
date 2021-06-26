5935f62d3b0b3f8fb15149747038ea057071f6ae
K-Phanudet <k.phanudet.work@gmail.com>
2021-06-26 17:13:22 +0700

    complete remove

--- myApp.js
+++ myApp.js
@@ -77,8 +77,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.deleteMany({name:nameToRemove},(err,data)=>{
+    if (err) return  console.error(err);
+    done(null ,data);
+  })
 };
 
 const queryChain = (done) => {
